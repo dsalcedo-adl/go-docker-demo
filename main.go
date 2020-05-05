4910813bcbd36934bc2f7a0a45661da017ee0275
Dennis Joseph Salcedo Perdomo <dennis.salcedo@ADL00065.local>
2020-05-05 12:03:11 -0500

    initial commit for go and docker

--- main.go
+++ main.go
@@ -1,19 +1,39 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 )
+
+type test_struct struct {
+	Test string
+}
 
 func main() {
 	// Set the flags for the logging package to give us the filename in the logs
 	log.SetFlags(log.LstdFlags | log.Lshortfile)
 
 	log.Println("starting server...")
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	http.HandleFunc("/test", func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusOK)
-		_, _ = fmt.Fprintln(w, `Hello, visitor!`)
+//		_, _ = fmt.Fprintln(w, `Hello, visitor!`)
+		decoder := json.NewDecoder(r.Body)
+		var t test_struct
+		err := decoder.Decode(&t)
+		if err != nil {
+			panic(err)
+		}
+		var jsonData []byte
+		jsonData, err = json.Marshal(t)
+		if err != nil {
+			log.Println(err)
+		}
+		//log.Println(t)
+		//_, _ = fmt.Fprintln(w, t)
+		log.Println(string(jsonData))
+		_, _ = fmt.Fprintln(w, string(jsonData))
 	})
 	log.Fatal(http.ListenAndServe(":8000", nil))
 }
